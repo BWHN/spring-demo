dfc63afb4a7cc5440134533f4f9cffcd7f996fa8
hunan <1369165806@qq.com>
2024-07-13 20:53:06 +0800

    还原删除的 BeanFactory 功能

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,8 +10,14 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
+
+        // BeanFactory 功能
+        Object a = context.getBean("a");
+        System.out.println(a);
+
         // MessageResource 功能
         System.out.println(context.getMessage("hi", null, Locale.ENGLISH));
         System.out.println(context.getMessage("hi", null, Locale.CHINA));
+
     }
 }
