fe1915edca99d63abf45f0450ee1a2a48efae975
hunan <1369165806@qq.com>
2024-07-15 20:56:41 +0800

    增加ResourcePatternResolver使用

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,14 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.io.Resource;
 
+import java.io.IOException;
 import java.util.Locale;
 
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
 
         // BeanFactory 功能
@@ -21,6 +23,12 @@ public class Main {
 
         // Environment 功能
         System.out.println(context.getEnvironment().getProperty("spring.server.port"));
+
+        // ResourcePatternResolver 功能
+        Resource[] resources = context.getResources("classpath*:META-INF/spring.factories");
+        for (Resource resource : resources) {
+            System.out.println(resource);
+        }
 
     }
 }
