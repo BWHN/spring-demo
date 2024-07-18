a9986c65f926dabbff1c2902195197c3e252c3a4
hunan <1369165806@qq.com>
2024-07-18 22:32:28 +0800

    增加BeanDefinition生成

--- src/main/java/org/example/beanFactory/BeanFactoryTest.java
+++ src/main/java/org/example/beanFactory/BeanFactoryTest.java
@@ -1,14 +1,32 @@
 package org.example.beanFactory;
 
+import org.springframework.beans.factory.BeanFactoryUtils;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 public class BeanFactoryTest {
 
     public static void main(String[] args) {
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
-        
+
+        AbstractBeanDefinition singleton = BeanDefinitionBuilder.genericBeanDefinition(MyConfig.class).setScope("singleton").getBeanDefinition();
 
 
     }
+
+    @Configuration
+    static class MyConfig {
+
+        @Bean
+        public Bean1 bean1() {
+            return new Bean1();
+        }
+
+    }
+
+    static class Bean1 {}
 
 }
