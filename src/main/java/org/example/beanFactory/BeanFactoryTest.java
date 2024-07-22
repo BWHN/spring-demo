4ca5ecff80113a95e262aad37d2d1fdae03b3d9e
hunan <1369165806@qq.com>
2024-07-22 22:39:45 +0800

    add BeanFactoryPostProcessor

--- src/main/java/org/example/beanFactory/BeanFactoryTest.java
+++ src/main/java/org/example/beanFactory/BeanFactoryTest.java
@@ -1,11 +1,15 @@
 package org.example.beanFactory;
 
 import org.springframework.beans.factory.BeanFactoryUtils;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotationUtils;
 
 public class BeanFactoryTest {
 
@@ -15,6 +19,10 @@ public class BeanFactoryTest {
         AbstractBeanDefinition singleton = BeanDefinitionBuilder.genericBeanDefinition(MyConfig.class).setScope("singleton").getBeanDefinition();
 
         beanFactory.registerBeanDefinition("myConfig", singleton);
+
+        AnnotationConfigUtils.registerAnnotationConfigProcessors(beanFactory);
+
+        beanFactory.getBeansOfType(BeanFactoryPostProcessor.class).values().forEach(e -> e.postProcessBeanFactory(beanFactory));
 
         beanFactory.getBeanNamesIterator().forEachRemaining(System.out::println);
 
