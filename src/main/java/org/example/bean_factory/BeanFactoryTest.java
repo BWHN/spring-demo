f6cbd9c52fed7a3744bdf3be745ac11e25c50e0d
hunan <1369165806@qq.com>
2024-07-27 23:12:43 +0800

    增加注释

--- src/main/java/org/example/bean_factory/BeanFactoryTest.java
+++ src/main/java/org/example/bean_factory/BeanFactoryTest.java
@@ -10,14 +10,15 @@ import org.springframework.context.annotation.AnnotationConfigUtils;
 public class BeanFactoryTest {
 
     public static void main(String[] args) {
+
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
-
+        // 构建beanDefinition
         AbstractBeanDefinition singleton = BeanDefinitionBuilder.genericBeanDefinition(MyConfig.class).setScope("singleton").getBeanDefinition();
-
+        // 注册beanDefinition
         beanFactory.registerBeanDefinition("myConfig", singleton);
-
+        // 向bean工厂加入Bean处理器
         AnnotationConfigUtils.registerAnnotationConfigProcessors(beanFactory);
-
+        // 应用Bean处理器
         beanFactory.getBeansOfType(BeanFactoryPostProcessor.class).values().forEach(e -> e.postProcessBeanFactory(beanFactory));
 
         beanFactory.getBeanNamesIterator().forEachRemaining(System.out::println);
